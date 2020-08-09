8664a28668fd32768446c3b36f489a10206f7d9f
zer0ken <a37592777@gmail.com>
2020-08-09 21:45:48 +0900

    ~ commands about application in ShteloCog

--- extensions/shtelo_cog.py
+++ extensions/shtelo_cog.py
@@ -16,10 +16,11 @@ INVITER = 5
 KNOWLEDGE = 6
 TWITTER_ID = 7
 STATE = 13
+REMARKS = 14
 HOUR = 3600
-RECEIVED = 'RECEIVED'
-APPROVED = 'APPROVED'
-REJECTED = 'REJECTED'
+RECEIVED = '접수됨'
+APPROVED = '승인됨'
+REJECTED = '거부됨'
 
 
 class FreshData:
@@ -28,7 +29,17 @@ class FreshData:
         self.timestamp = datetime.now()
 
 
-def get_state(application: list):
+def get_application_sheet():
+    constant = get_constant('application')
+    rows = sheet_read(constant['sheet_id'], constant['read_range'])
+    keys = rows.pop(0)
+    for reply in rows:
+        while len(reply) < len(keys):
+            reply.append('')
+    return keys, rows
+
+
+def state_of_application(application: list):
     literal = literals('get_state')
     if application[STATE] == RECEIVED:
         return literal['received']
@@ -39,6 +50,40 @@ def get_state(application: list):
     return ''
 
 
+def get_application_embed(data: list):
+    literal = literals('get_application_embed')
+    discord_id = data[DISCORD_ID]
+    title = literal['title'] % discord_id + state_of_application(data)
+    embeds = ChainedEmbed(title=title,
+                          description=literal['description'] % (data[TIMESTAMP], data[EMAIL]))
+    if data[SUBACCOUNT]:
+        embeds.add_field(name=literal['subaccount'], value=literal['mainaccount'] % data[SUBACCOUNT])
+    if data[NICKNAME]:
+        embeds.add_field(name=literal['nickname'], value='**' + data[NICKNAME] + '**')
+    if data[DISCORD_ID]:
+        embeds.add_field(name=literal['discord_id'], value='`' + data[DISCORD_ID] + '`', inline=True)
+    if data[TWITTER_ID]:
+        embeds.add_field(name=literal['twitter_id'], value='`' + data[TWITTER_ID] + '`', inline=True)
+    if data[INVITER]:
+        embeds.add_field(name=literal['inviter'], value='`' + data[INVITER] + '`', inline=True)
+    if data[KNOWLEDGE]:
+        embeds.add_field(name=literal['knowledge'], value='```\n' + data[KNOWLEDGE] + '\n```')
+    if data[REMARKS]:
+        embeds.add_field(name=literal['remarks'], value='```\n' + data[REMARKS] + '\n```')
+    return embeds
+
+
+def get_application_raw_embed(keys: list, data: list):
+    literal = literals('get_application_raw_embed')
+    discord_id = data[DISCORD_ID]
+    title = literal['title'] % discord_id + state_of_application(data)
+    embeds = ChainedEmbed(title=title, description=literal['description'])
+    for i in range(len(keys)):
+        if data[i]:
+            embeds.add_field(name=keys[i], value='```\n' + data[i] + '\n```')
+    return embeds
+
+
 class ShteloCog(CustomCog, name=get_cog('ShteloCog')['name']):
     """
     슈텔로의 관리를 위한 기능을 포함합니다.
@@ -47,59 +92,19 @@ class ShteloCog(CustomCog, name=get_cog('ShteloCog')['name']):
     def __init__(self, client: Bot):
         super().__init__(client)
         self.client: Bot = client
-        self.application_keys: list = []  # table keys of application spreadsheet
         self.regulation: Optional[FreshData] = None  # recently loaded regulation
-
-    def get_sheet(self):
-        constant = get_constant('application')
-        replies = sheet_read(constant['sheet_id'], constant['read_range'])
-        self.application_keys = replies.pop(0)
-        for reply in replies:
-            if len(reply) <= STATE:
-                reply.append('')
-        return replies
-
-    def get_application_embed(self, data: list):
-        literal = literals('get_application_embed')
-        discord_id = data[DISCORD_ID]
-        title = literal['title'] % discord_id + get_state(data)
-        embeds = ChainedEmbed(title=title,
-                              description=literal['description'] % (data[TIMESTAMP], data[EMAIL]))
-        if data[SUBACCOUNT]:
-            embeds.add_field(name=literal['subaccount'], value=literal['mainaccount'] % data[SUBACCOUNT])
-        if data[NICKNAME]:
-            embeds.add_field(name=literal['nickname'], value='**' + data[NICKNAME] + '**')
-        if data[DISCORD_ID]:
-            embeds.add_field(name=literal['discord_id'], value='`' + data[DISCORD_ID] + '`', inline=True)
-        if data[TWITTER_ID]:
-            embeds.add_field(name=literal['twitter_id'], value='`' + data[TWITTER_ID] + '`', inline=True)
-        if data[INVITER]:
-            embeds.add_field(name=literal['inviter'], value='`' + data[INVITER] + '`', inline=True)
-        if data[KNOWLEDGE]:
-            embeds.add_field(name=literal['knowledge'], value='```\n' + data[KNOWLEDGE] + '\n```')
-        return embeds
-
-    def get_application_raw_embed(self, data: list):
-        literal = literals('get_application_raw_embed')
-        discord_id = data[DISCORD_ID]
-        title = literal['title'] % discord_id + get_state(data)
-        embeds = ChainedEmbed(title=title, description=literal['description'])
-        for i in range(len(self.application_keys) - 1):
-            if data[i]:
-                embeds.add_field(name=self.application_keys[i], value='```\n' + data[i] + '\n```')
-        return embeds
 
     @modules.group(name='가입신청서', aliases=('가입', '신청서'))
     async def applications(self, ctx: Context):
         literal = literals('applications')
         message = await ctx.send(literal['start'])
-        replies = self.get_sheet()
+        _, replies = get_application_sheet()
         count = len(replies)
         for reply in replies:
             if reply[STATE]:
                 count -= 1
                 continue
-            embeds = self.get_application_embed(reply)
+            embeds = get_application_embed(reply)
             for embed in embeds.to_list():
                 await ctx.author.send(embed=embed)
         await message.edit(content=literal['done'] % count if count else literal['not_found'])
@@ -108,10 +113,10 @@ class ShteloCog(CustomCog, name=get_cog('ShteloCog')['name']):
     async def applications_all(self, ctx: Context):
         literal = literals('applications_all')
         message = await ctx.send(literal['start'])
-        replies = self.get_sheet()
+        _, replies = get_application_sheet()
         count = len(replies)
         for reply in replies:
-            embeds = self.get_application_embed(reply)
+            embeds = get_application_embed(reply)
             for embed in embeds.to_list():
                 await ctx.author.send(embed=embed)
         await message.edit(content=literal['done'] % count if count else literal['not_found'])
@@ -120,13 +125,13 @@ class ShteloCog(CustomCog, name=get_cog('ShteloCog')['name']):
     async def application_raw(self, ctx: Context):
         literal = literals('application_raw')
         message = await ctx.send(literal['start'])
-        replies = self.get_sheet()
+        keys, replies = get_application_sheet()
         count = len(replies)
         for reply in replies:
             if reply[STATE]:
                 count -= 1
                 continue
-            embeds = self.get_application_raw_embed(reply)
+            embeds = get_application_raw_embed(keys, reply)
             for embed in embeds.to_list():
                 await ctx.author.send(embed=embed)
         await message.edit(content=literal['done'] % count if count else literal['not_found'])
@@ -135,10 +140,10 @@ class ShteloCog(CustomCog, name=get_cog('ShteloCog')['name']):
     async def application_raw_all(self, ctx: Context):
         literal = literals('application_raw_all')
         message = await ctx.send(literal['start'])
-        replies = self.get_sheet()
+        keys, replies = get_application_sheet()
         count = len(replies)
         for reply in replies:
-            embeds = self.get_application_raw_embed(reply)
+            embeds = get_application_raw_embed(keys, reply)
             for embed in embeds.to_list():
                 await ctx.author.send(embed=embed)
         await message.edit(content=literal['done'] % count if count else literal['not_found'])
