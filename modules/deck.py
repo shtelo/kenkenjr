366d25bd5b47e3fe366543373abb314ebb7be512
zer0ken <a37592777@gmail.com>
2020-08-27 07:08:23 +0900

    ~ deck module and commands in ShteloCog
    (cherry picked from commit 6b8a47ff873d82d35b6ffe82636af307548eddbd)

--- modules/deck.py
+++ modules/deck.py
@@ -17,18 +17,20 @@ def mention_to_id(mention: str) -> int:
 
 
 class Deck:
-    PUBLIC = ':white_check_mark:'
-    NSFW = ':underage:'
-    AUTO = ':robot:'
-    LOCK = ':lock:'
+    PUBLIC_EMOJI = ':white_check_mark:'
+    NSFW_EMOJI = ':underage:'
+    AUTO_EMOJI = ':robot:'
+    LOCK_EMOJI = ':lock:'
+    PENDING_EMOJI = ':raised_hand:'
+
     MANAGER = '매니저'
-    PENDING = '보류된 가입'
+    PENDING = '가입신청자'
 
     def __init__(self, **kwargs):
-        self.public: bool = kwargs.get('public')
-        self.nsfw: bool = kwargs.get('nsfw')
-        self.auto: bool = kwargs.get('auto')
-        self.lock: bool = kwargs.get('lock')
+        self.public: bool = self.PUBLIC_EMOJI in kwargs.get('settings')
+        self.nsfw: bool = self.NSFW_EMOJI in kwargs.get('settings')
+        self.auto: bool = self.AUTO_EMOJI in kwargs.get('settings')
+        self.lock: bool = self.LOCK_EMOJI in kwargs.get('settings')
         self.id: str = kwargs.get('id')
         self.manager: Member = kwargs.get('manager')
         self.pending: list = kwargs.get('pending')
@@ -39,23 +41,22 @@ class Deck:
         self.role: Role = kwargs.get('role')
 
     def __repr__(self):
-        return f'<Deck public={self.public} nswf={self.nsfw} auto={self.auto} id={self.id} manager={self.manager} ' \
-               f'pending={self.pending} name={self.name} topic={self.topic} chategory_channel={self.category_channel}' \
-               f' default_channel={self.default_channel} role={self.role}>'
+        return f'<Deck public={self.public} nswf={self.nsfw} auto={self.auto} lock={self.lock} id={self.id} ' \
+               f'manager={self.manager} pending={self.pending} name={self.name} topic={self.topic} ' \
+               f'chategory_channel={self.category_channel} default_channel={self.default_channel} role={self.role}>'
 
     def to_channel_topic(self):
-        deck_str = '​'
-        setting = []
+        deck_str = '*id: ' + self.id
+        setting = list()
         if self.public:
-            setting.append(self.PUBLIC)
+            setting.append(self.PUBLIC_EMOJI)
         if self.nsfw:
-            setting.append(self.NSFW)
+            setting.append(self.NSFW_EMOJI)
         if self.auto:
-            setting.append(self.AUTO)
+            setting.append(self.AUTO_EMOJI)
         if self.lock:
-            setting.append(self.LOCK)
-        deck_str += (' '.join(setting) + '\n' if setting else '')
-        deck_str += 'id: ' + self.id
+            setting.append(self.LOCK_EMOJI)
+        deck_str += ('\n' + ' '.join(setting) if setting else '')
         deck_str += '\n' + self.MANAGER + ': ' + self.manager.mention
         deck_str += ('\n' + self.PENDING + ': ' + '\n'.join([member.mention for member in self.pending])
                      if self.pending else '')
@@ -67,29 +68,26 @@ class Deck:
                + '***`' + self.id + '`*** ' \
                + '**__' + self.name + '__** ' \
                + '@' + str(self.manager) \
-               + (' ' + self.PUBLIC if self.public else '') \
-               + (' ' + self.NSFW if self.nsfw else '') \
-               + (' ' + self.AUTO if self.auto else '') \
-               + (' ' + self.LOCK if self.lock else '')
+               + (' ' + self.PUBLIC_EMOJI if self.public else '') \
+               + (' ' + self.NSFW_EMOJI if self.nsfw else '') \
+               + (' ' + self.AUTO_EMOJI if self.auto else '') \
+               + (' ' + self.LOCK_EMOJI if self.lock else '') \
+               + (' ' + self.PENDING_EMOJI if self.pending else '')
 
 
 @singleton
 class DeckHandler:
     SHTELO_ID = 650533223520010261
 
-    PUBLIC_REGEX = '(:white_check_mark:)'
-    NSFW_REGEX = '(:underage:)'
-    AUTO_REGEX = '(:robot:)'
-    LOCK_REGEX = '(:lock:)'
-    SETTING_REGEX = '({0}|{1}|{2}|{3})'.format(PUBLIC_REGEX, NSFW_REGEX, AUTO_REGEX, LOCK_REGEX)
-
-    ID_REGEX = '(id: [0-9a-zA-Z]{4,})'
-    MENTION_REGEX = '(<@!?\\d+>)'
-    MANAGER_REGEX = '(\\n매니저: {0})'.format(MENTION_REGEX)
-    PENDING_REGEX = '(\\n보류된 가입:( {0})+)'.format(MENTION_REGEX)
-    TOPIC_REGEX = '(\\n\\n(.|\\n)+$)'
-    FULL_REGEX = '^​({0}( {0})*\\n)?{1}{2}({3})?({4}|$)' \
-        .format(SETTING_REGEX, ID_REGEX, MANAGER_REGEX, PENDING_REGEX, TOPIC_REGEX)
+    MENTION_REGEX = '<@!?\\d+>'
+    ID_REGEX = '\\*id: [a-zA-Z0-9]{4,}'
+    SETTING_REGEX = '({0}|{1}|{2}|{3})( ({0}|{1}|{2}|{3}))*'\
+        .format(Deck.PUBLIC_EMOJI, Deck.NSFW_EMOJI, Deck.AUTO_EMOJI, Deck.LOCK_EMOJI)
+    MANAGER_REGEX = '매니저: {0}'.format(MENTION_REGEX)
+    PENDING_REGEX = '가입신청자:( {0})+'.format(MENTION_REGEX)
+    TOPIC_REGEX = '(.|\\n)+'
+    ENTIRE_REGEX = '^{0}(\\n{1})?\\n{2}(\\n{3})?(\\n{4})?$' \
+        .format(ID_REGEX, SETTING_REGEX, MANAGER_REGEX, PENDING_REGEX, TOPIC_REGEX)
 
     def __init__(self, client: Client):
         self.client: Client = client
@@ -123,7 +121,7 @@ class DeckHandler:
             if isinstance(channel, TextChannel) \
                     and channel.category_id is not None \
                     and channel.topic is not None \
-                    and match(self.FULL_REGEX, channel.topic) is not None:
+                    and match(self.ENTIRE_REGEX, channel.topic) is not None:
                 tasks.append(self.__fetch_deck__(channel))
         if tasks:
             await asyncio.wait(tasks)
@@ -134,40 +132,36 @@ class DeckHandler:
         self.ready = True
 
     async def __fetch_deck__(self, default_channel: TextChannel):
-        raw = default_channel.topic[1:]
-        category_channel = await self.client.fetch_channel(default_channel.category_id)
+        if (category_id := default_channel.category_id) in self.decks:
+            del self.decks[category_id]
+        category_channel = await self.client.fetch_channel(category_id)
         deck_name = category_channel.name
         deck_role = discord.utils.get(await self.guild.fetch_roles(), name=deck_name)
-        deck_public = Deck.PUBLIC in (firstline := default_channel.topic.split('\n', 1)[0])
-        deck_nsfw = Deck.NSFW in firstline
-        deck_auto = Deck.AUTO in firstline
-        deck_lock = Deck.LOCK in firstline
-        if any((deck_public, deck_nsfw, deck_auto, deck_lock)):
-            raw = '\n'.join(raw.split('\n')[1:])
-        deck_id = findall('^' + self.ID_REGEX, raw)[0]
-        raw = raw[len(deck_id):]
-        deck_id = deck_id[4:]
-        deck_manager = findall('^' + self.MANAGER_REGEX, raw)[0][0]
-        raw = raw[len(deck_manager):]
-        deck_manager = await self.guild.fetch_member(mention_to_id(deck_manager.split(' ', 1)[1]))
-        if deck_pending := findall('^' + self.PENDING_REGEX, raw):
-            raw = raw[len(deck_pending[0][0]):]
-            deck_pending = [await self.guild.fetch_member(mention_to_id(mention))
-                            for mention in deck_pending[0][0].split()[2:]]
-        if deck_topic := findall('^' + self.TOPIC_REGEX, raw):
-            deck_topic = deck_topic[0][0][2:]
-        self.decks[category_channel.id] = Deck(public=deck_public,
-                                               nsfw=deck_nsfw,
-                                               auto=deck_auto,
-                                               lock=deck_lock,
-                                               id=deck_id,
-                                               manager=deck_manager,
-                                               pending=deck_pending,
-                                               name=deck_name,
-                                               topic=deck_topic,
-                                               category_channel=category_channel,
-                                               default_channel=default_channel,
-                                               role=deck_role)
+        raw = default_channel.topic
+        deck_id = match('^' + self.ID_REGEX, raw).group()[5:]
+        raw = raw.split('\n', 1)[-1]
+        if deck_settings := match('^' + self.SETTING_REGEX, raw):
+            deck_settings = deck_settings.group().split()
+            raw = raw.split('\n', 1)[-1]
+        else:
+            deck_settings = tuple()
+        deck_manager = await self.guild.fetch_member(mention_to_id(match('^' + self.MANAGER_REGEX, raw).group()[5:]))
+        if '\n' in raw:
+            raw = raw.split('\n', 1)[-1]
+        deck_pending = list()
+        if pending := match('^' + self.PENDING_REGEX, raw):
+
+            async def append_after_fetch(member_id):
+                deck_pending.append(await self.guild.fetch_member(member_id))
+
+            await asyncio.wait([append_after_fetch(mention_to_id(member)) for member in pending.group()[8:].split()])
+            if '\n' in raw:
+                raw = raw.split('\n', 1)[-1]
+        deck_topic = raw.split('\n', 1)[-1] if '\n' in raw else ''
+        deck = Deck(settings=deck_settings, default_channel=default_channel, category_channel=category_channel,
+                    pending=deck_pending, topic=deck_topic, id=deck_id, manager=deck_manager, name=deck_name,
+                    role=deck_role)
+        self.decks[category_id] = deck
 
     @staticmethod
     async def save_deck(deck: Deck):
